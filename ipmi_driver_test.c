691ba0a5ec4c1bf0524a6fc21809aee1e296f08d
Corey Minyard <corey@minyard.net>
2025-04-09 12:21:19 -0500

    Add basic test framework
    Signed-off-by: Corey Minyard <corey@minyard.net>

---
+++ ipmi_driver_test.c
@@ -0,0 +1,413 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <gensio/gensio.h>
+#include <gensio/gensio_openipmi_oshandler.h>
+#include <gensio/gensio_list.h>
+#include <OpenIPMI/ipmiif.h>
+#include <OpenIPMI/ipmi_conn.h>
+#include <OpenIPMI/internal/ipmi_event.h>
+
+struct sendbuf {
+    struct gensio_link link;
+    gensiods len;
+    gensiods pos;
+    unsigned char *data;
+};
+
+struct tinfo {
+    int rv;
+
+    struct gensio_os_funcs *o;
+    struct gensio *helper;
+
+    os_handler_t *oo;
+    ipmi_con_t *icon;
+
+    struct gensio_waiter *waiter;
+
+    bool ready;
+    bool closing;
+    unsigned int close_wait_count;
+
+    char inbuf[1024];
+    gensiods inbuf_len;
+
+    /* List of struct sendbuf to write. */
+    struct gensio_list writelist;
+
+};
+
+static void start_test_close(struct tinfo *ti);
+
+static void
+do_vlog(struct gensio_os_funcs *f, enum gensio_log_levels level,
+	const char *log, va_list args)
+{
+    fprintf(stderr, "gensio %s log: ", gensio_log_level_to_str(level));
+    vfprintf(stderr, log, args);
+    fprintf(stderr, "\n");
+}
+
+void
+ipmi_event_handler(ipmi_con_t        *ipmi,
+		   const ipmi_addr_t *addr,
+		   unsigned int      addr_len,
+		   ipmi_event_t      *event,
+		   void              *cb_data)
+{
+    unsigned int        record_id = ipmi_event_get_record_id(event);
+    unsigned int        type = ipmi_event_get_type(event);
+    unsigned int        data_len = ipmi_event_get_data_len(event);
+    const unsigned char *data = ipmi_event_get_data_ptr(event);
+    unsigned int        i;
+
+    printf("Got event:\n");
+    printf("  %4.4x (%2.2x):", record_id, type);
+    for (i=0; i<data_len; i++)
+	printf(" %2.2x", data[i]);
+    printf("\n");
+}
+
+static void
+handle_buf(struct tinfo *ti)
+{
+}
+
+static int
+io_event(struct gensio *io, void *user_data, int event, int err,
+	 unsigned char *buf, gensiods *buflen,
+	 const char *const *auxdata)
+{
+    struct tinfo *ti = user_data;
+    gensiods len, i;
+    int rv;
+    bool handle_it = false;
+
+    switch (event) {
+    case GENSIO_EVENT_READ:
+	if (ti->closing)
+	    return 0;
+
+	if (err) {
+	    fprintf(stderr, "Error from helper: %s\n", gensio_err_to_str(err));
+	    ti->rv = 1;
+	    start_test_close(ti);
+	    return 0;
+	}
+
+	len = *buflen;
+	for (i = 0; i < len; i++) {
+	    if (buf[i] == '\n' || buf[i] == '\r') {
+		ti->inbuf[ti->inbuf_len] = '\0';
+		ti->inbuf_len = 0;
+		/*
+		 * Note that you could continue to process characters
+		 * but this demonstrates that you can process partial
+		 * buffers, which can sometimes simplify code.
+		 */
+		handle_it = true;
+		i++;
+		break;
+	    }
+	    if (ti->inbuf_len >= sizeof(ti->inbuf) - 1)
+		continue;
+	    ti->inbuf[ti->inbuf_len++] = buf[i];
+	}
+	*buflen = i; /* We processed the characters up to the new line. */
+
+	if (handle_it && ti->inbuf_len > 0)
+	    handle_buf(ti);
+	return 0;
+
+    case GENSIO_EVENT_WRITE_READY:
+	if (ti->closing) {
+	    gensio_set_write_callback_enable(ti->helper, false);
+	    return 0;
+	}
+
+	while (!gensio_list_empty(&ti->writelist)) {
+	    struct gensio_link *l = gensio_list_first(&ti->writelist);
+	    struct sendbuf *sb = gensio_container_of(l, struct sendbuf, link);
+
+	    rv = gensio_write(ti->helper, &i,
+			      sb->data + sb->pos, sb->len - sb->pos,
+			      NULL);
+	    if (rv) {
+		if (rv != GE_REMCLOSE)
+		    fprintf(stderr, "Error writing to io: %s\n",
+			    gensio_err_to_str(rv));
+		gensio_set_write_callback_enable(ti->helper, false);
+		ti->rv = 1;
+		start_test_close(ti);
+		return 0;
+	    }
+	    sb->pos += i;
+	    if (sb->pos >= sb->len) {
+		gensio_list_rm(&ti->writelist, &sb->link);
+		gensio_os_funcs_zfree(ti->o, sb);
+	    } else {
+		break;
+	    }
+	}
+	if (gensio_list_empty(&ti->writelist))
+	    gensio_set_write_callback_enable(ti->helper, false);
+	return 0;
+
+    default:
+	return GE_NOTSUP;
+    }
+}
+
+static void
+ipmi_con_changed_handler(ipmi_con_t   *con,
+			 int          err,
+			 unsigned int port_num,
+			 int          still_connected,
+			 void         *cb_data)
+{
+    struct tinfo *ti = cb_data;
+
+    if (ti->closing)
+	return;
+
+    if (err) {
+	fprintf(stderr, "IPMI connection failure: %x\n", err);
+	ti->icon = NULL;
+	con->close_connection(con);
+	if (ti->ready)
+	    start_test_close(ti);
+    }
+
+    gensio_os_funcs_wake(ti->o, ti->waiter);
+}
+
+static void
+helper_open_done(struct gensio *io, int err, void *open_data)
+{
+    struct tinfo *ti = open_data;
+
+    if (ti->closing)
+	return;
+
+    if (err) {
+	fprintf(stderr, "helper connection failure: %s\n",
+		gensio_err_to_str(err));
+	gensio_free(io);
+	ti->helper = NULL;
+    }
+
+    gensio_os_funcs_wake(ti->o, ti->waiter);
+}
+
+static struct {
+    char *name;
+    char *value;
+} ipmi_con_args[] = {
+    { "Address", "localhost" },
+    { "Port", "9001" },
+    { "Username", "ipmiusr" },
+    { "Password", "test" },
+    {}
+};
+
+static int
+ipmi_setup_con(struct tinfo *ti)
+{
+    ipmi_args_t *iargs;
+    int rv;
+    unsigned int i;
+
+    ti->oo = gensio_openipmi_oshandler_alloc(ti->o);
+    if (!ti->oo) {
+	fprintf(stderr, "Could not allocate openipmi os handler\n");
+	return 1;
+    }
+
+    rv = ipmi_init(ti->oo);
+    if (rv) {
+	fprintf(stderr, "Error initializing connections: 0x%x\n", rv);
+	return 1;
+    }
+
+    rv = ipmi_args_alloc("lan", &iargs);
+    if (rv) {
+	fprintf(stderr, "Error allocating IPMI connection args: 0x%x\n", rv);
+	return 1;
+    }
+
+    for (i = 0; ipmi_con_args[i].name; i++) {
+	rv = ipmi_args_set_val(iargs, -1,
+			       ipmi_con_args[i].name, ipmi_con_args[i].value);
+	if (rv) {
+	    fprintf(stderr, "Error setting IPMI con arg %s(%s): 0x%x\n",
+		    ipmi_con_args[i].name, ipmi_con_args[i].value, rv);
+	    return 1;
+	}
+    }
+
+    rv = ipmi_args_setup_con(iargs, ti->oo, NULL, &ti->icon);
+    if (rv) {
+	fprintf(stderr, "Error setting up IPMI connection: 0x%x\n", rv);
+	return 1;
+    }
+    ipmi_free_args(iargs);
+
+    rv = ti->icon->add_event_handler(ti->icon, ipmi_event_handler, ti);
+    if (rv) {
+	fprintf(stderr, "Error setting up event handler: 0x%x\n", rv);
+	return 1;
+    }
+
+    ti->icon->add_con_change_handler(ti->icon, ipmi_con_changed_handler, ti);
+
+    rv = ti->icon->start_con(ti->icon);
+    if (rv) {
+	fprintf(stderr, "Could not start connection: %x\n", rv);
+	return 1;
+    }
+
+    return 0;
+}
+
+static int
+helper_setup_con(struct tinfo *ti)
+{
+    int rv;
+
+    rv = str_to_gensio("tcp,localhost,2000", ti->o, io_event, ti, &ti->helper);
+    if (rv) {
+	fprintf(stderr, "Could not allocate gensio: %s\n",
+		gensio_err_to_str(rv));
+	return 1;
+    }
+
+    rv = gensio_open(ti->helper, helper_open_done, ti);
+    if (rv) {
+	fprintf(stderr, "Could not open gensio: %s\n", gensio_err_to_str(rv));
+	return 1;
+    }
+
+    return 0;
+}
+
+static void
+ipmi_close_done(ipmi_con_t *ipmi, void *cb_data)
+{
+    struct tinfo *ti = cb_data;
+
+    ti->close_wait_count--;
+    if (ti->close_wait_count == 0)
+	gensio_os_funcs_wake(ti->o, ti->waiter);
+}
+
+static void
+helper_close_done(struct gensio *io, void *close_data)
+{
+    struct tinfo *ti = close_data;
+
+    ti->close_wait_count--;
+    if (ti->close_wait_count == 0)
+	gensio_os_funcs_wake(ti->o, ti->waiter);
+}
+
+static void
+start_test_close(struct tinfo *ti)
+{
+    int rv;
+
+    ti->closing = true;
+
+    if (ti->icon) {
+	rv = ti->icon->close_connection_done(ti->icon, ipmi_close_done, ti);
+	if (!rv)
+	    ti->close_wait_count++;
+    }
+
+    if (ti->helper) {
+	rv = gensio_close(ti->helper, helper_close_done, ti);
+	if (rv)
+	    gensio_free(ti->helper);
+	else
+	    ti->close_wait_count++;
+    }
+
+    if (ti->close_wait_count == 0)
+	gensio_os_funcs_wake(ti->o, ti->waiter);
+}
+
+int
+main(int argc, char *argv[])
+{
+    struct tinfo ti;
+    struct gensio_os_proc_data *proc_data = NULL;
+    int rv;
+    gensio_time timeout;
+
+    memset(&ti, 0, sizeof(ti));
+
+    rv = gensio_alloc_os_funcs(GENSIO_DEF_WAKE_SIG, &ti.o, 0);
+    if (rv) {
+	fprintf(stderr, "Could not allocate OS handler: %s\n",
+		gensio_err_to_str(rv));
+	return 1;
+    }
+    gensio_os_funcs_set_vlog(ti.o, do_vlog);
+
+    rv = gensio_os_proc_setup(ti.o, &proc_data);
+    if (rv) {
+	fprintf(stderr, "Could not setup process data: %s\n",
+		gensio_err_to_str(rv));
+	ti.rv = 1;
+	goto out_close;
+    }
+
+    ti.waiter = gensio_os_funcs_alloc_waiter(ti.o);
+    if (!ti.waiter) {
+	fprintf(stderr, "Could not allocate waiter, out of memory\n");
+	ti.rv = 1;
+	goto out_close;
+    }
+
+    if (ipmi_setup_con(&ti)) {
+	ti.rv = 1;
+	goto out_close;
+    }
+
+    if (helper_setup_con(&ti)) {
+	ti.rv = 1;
+	start_test_close(&ti);
+	goto out_wait_close;
+    }
+
+    timeout.secs = 2;
+    timeout.nsecs = 0;
+    rv = gensio_os_funcs_wait(ti.o, ti.waiter, 2, &timeout);
+    if (rv) {
+	fprintf(stderr, "Error setting up connections: %s\n",
+		gensio_err_to_str(rv));
+	ti.rv = 1;
+	goto out_close;
+    }
+
+    ti.ready = true;
+
+    start_test_close(&ti);
+
+ out_wait_close:
+    rv = gensio_os_funcs_wait(ti.o, ti.waiter, 1, NULL);
+    if (rv) {
+	fprintf(stderr, "Error from wait: %s\n",
+		gensio_err_to_str(rv));
+	ti.rv = 1;
+    }
+
+ out_close:
+    if (ti.waiter)
+	gensio_os_funcs_free_waiter(ti.o, ti.waiter);
+    if (proc_data)
+	gensio_os_proc_cleanup(proc_data);
+    gensio_os_funcs_free(ti.o);
+
+    return ti.rv;
+}
